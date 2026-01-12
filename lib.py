95ed2fc76410e6c603aa355dc3ba1a657c429683
pheathtwilio <72936603+pheathtwilio@users.noreply.github.com>
2026-01-12 15:04:13 -0600

    Adding back propagation for one neural node

--- lib.py
+++ lib.py
@@ -5,20 +5,77 @@ def sigmoid(z: float) -> float:
 
 def forward(x, w, b):
     """
+    Forward pass and calculate the sigmoid for a given list of inputs, weights and bias
     x: list of floats, length n - input
     w: list of floats, length n - weights
     b: float - bias
     returns: (z, a)
     """
     z = 0.0
+
     print("zip(x, w) ", list(zip(x, w)))
-    for xi, wi in zip(x, w): # all zip does is puts the first input with the first weight ad infinitum
+    for xi, wi in zip(x, w): # zip puts the first input with the first weight ad infinitum
         z += wi * xi
     z += b
+
     print("compute a weighted sum z = sum(wi, xi) + b = ", z)
     a = sigmoid(z)
+
     print("activation function ", a) # converts a real number (z) into a 1 or 0
     return z, a
 
+def backward(x, y, a):
+    """
+    Backward pass, determine how should each weight and the bias change to make the loss smaller
+    x: list of floats, length n - input
+    y: float, the target
+    a: the prediction (the output of the function after sigmoid)
+    returns dL_dw (list of gradients), dL_db (a single gradient for the bias)
+    """
+
+    # if a > y, the (a - y) is positive, the loss will decrease if a goes down, if a < y then (a - y) is negative, the loss will decrease if a goes up
+    dL_da = 2.0 * (a - y) # multiplied by 2 because we square for the loss facto
+    print("the direction dL_da is ", dL_da)
+    
+    # if a is near 0.5 sigmoid is sensitive (bigger slope), is a is near 0 or 1 so changing z barely changes a
+    da_dz = a * (1.0 - a)
+    print("how much the prediction changes if the weighted sum changes")
+
+    # how much does the loss change if we change z?
+    dL_dz = dL_da * da_dz
+    print("The loss changes by ", dL_dz, " if we change z")
+
+    # produce a gradient for each weight e.g. which way should we change it to reduce loss
+    dL_dw = [dL_dz * xi for xi in x]
+    print("The gradients for each weight", list(dL_dw))
+
+    # determine the gradient for the bias
+    dL_db = dL_dz
+    print("the gradient for the bias is ", dL_db)
+
+    return dL_dw, dL_db
+
+def update(w, b, dL_dw, dL_db, lr):
+    """
+    Update the weights and bias limited by the learning rate (steps to get to near zero loss)
+    w: list of current weights
+    b: current bias
+    dL_dw: list of gradients for each weight, should be same length as w
+    dL_db: gradient for bias
+    lr: learning rate
+    returns new list of weights and bias
+    """
+
+    # Update weights - for each weight wi subtract the learning rate from the gradient dwi, zip creates a list of weights with each gradient pair 
+    w = [wi - lr * dwi for wi, dwi in zip(w, dL_dw)]
+
+    # update bias - update the bias by multiplying the learning rate by the bias gradient
+    b = b - lr * dL_db
+    return w, b
+
 def mse(y, a):
+    """
+    return the mean squared error of the difference between the target y and the activation function a
+    """
     return (y - a) ** 2
+
--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-from examples import first_pass
+# from examples import first_pass
+import random
+from lib import forward, backward, update, mse
 
 if __name__ == "__main__":
     
-    # sample inputs, weights, bias and target for example first pass
-    x = [1.0, 0.0]
-    w = [0.2, -0.1]
-    b = 0.0
-    y = 1.0 # the target, the actual correct answer
+    random.seed(0)
 
-    z, a, loss = first_pass(x, w, b, y)
+    # training set
+    x = [1.0, 0.0]
+    y = 1.0
+
+    # random initialization
+    w = [random.uniform(-1, 1) for _ in range(len(x))]
+    b = 0.0
+    lr = 0.5
+
+    # before update
+    _, a1 = forward(x, w, b)
+    loss1 = mse(y, a1)
+
+    # compute gradients and updates
+    dL_dw, dL_db = backward(x, y, a1)
+    w2, b2 = update(w, b, dL_dw, dL_db, lr)
+
+    # after update
+    _, a2 = forward(x, w2, b2)
+    loss2 = mse(y, a2)
+
+    print("\n\n\nBefore:")
+    print(" w = ", w, "b = ", b)
+    print(" pred = ", a1, "loss = ", loss1)
+
+    print("\nGradients:")
+    print(" dL_dw = ", dL_dw)
+    print(" dL_db = ", dL_db)
+
+    print("\nAfter one update:")
+    print(" w = ", w2, " b = ", b2)
+    print(" pred = ", a2, " loss = ", loss2)
+
