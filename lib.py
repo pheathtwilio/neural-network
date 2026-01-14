6b2facb61c9697ce4c2dba3248129fd3cec9581e
pheathtwilio <72936603+pheathtwilio@users.noreply.github.com>
2026-01-14 14:23:26 -0600

    Added training function and prediction. This works for AND but not for XOR since it is not linearly separable

--- lib.py
+++ lib.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 def sigmoid(z: float) -> float:
     return 1.0 / (1.0 + math.exp(-z))
@@ -13,15 +14,15 @@ def forward(x, w, b):
     """
     z = 0.0
 
-    print("zip(x, w) ", list(zip(x, w)))
+    # print("zip(x, w) ", list(zip(x, w)))
     for xi, wi in zip(x, w): # zip puts the first input with the first weight ad infinitum
         z += wi * xi
     z += b
 
-    print("compute a weighted sum z = sum(wi, xi) + b = ", z)
+    # print("compute a weighted sum z = sum(wi, xi) + b = ", z)
     a = sigmoid(z)
 
-    print("activation function ", a) # converts a real number (z) into a 1 or 0
+    # print("activation function ", a) # converts a real number (z) into a 1 or 0
     return z, a
 
 def backward(x, y, a):
@@ -35,23 +36,23 @@ def backward(x, y, a):
 
     # if a > y, the (a - y) is positive, the loss will decrease if a goes down, if a < y then (a - y) is negative, the loss will decrease if a goes up
     dL_da = 2.0 * (a - y) # multiplied by 2 because we square for the loss facto
-    print("the direction dL_da is ", dL_da)
+    # print("the direction dL_da is ", dL_da)
     
     # if a is near 0.5 sigmoid is sensitive (bigger slope), is a is near 0 or 1 so changing z barely changes a
     da_dz = a * (1.0 - a)
-    print("how much the prediction changes if the weighted sum changes")
+    # print("how much the prediction changes if the weighted sum changes")
 
     # how much does the loss change if we change z?
     dL_dz = dL_da * da_dz
-    print("The loss changes by ", dL_dz, " if we change z")
+    # print("The loss changes by ", dL_dz, " if we change z")
 
     # produce a gradient for each weight e.g. which way should we change it to reduce loss
     dL_dw = [dL_dz * xi for xi in x]
-    print("The gradients for each weight", list(dL_dw))
+    # print("The gradients for each weight", list(dL_dw))
 
     # determine the gradient for the bias
     dL_db = dL_dz
-    print("the gradient for the bias is ", dL_db)
+    # print("the gradient for the bias is ", dL_db)
 
     return dL_dw, dL_db
 
@@ -72,6 +73,58 @@ def update(w, b, dL_dw, dL_db, lr):
     # update bias - update the bias by multiplying the learning rate by the bias gradient
     b = b - lr * dL_db
     return w, b
+
+def train(dataset, epochs=2000, lr=0.5, shuffle=True):
+    """
+    Training function
+    dataset: Array inputs and target 
+    epochs: number of times we are going to train the data. Default is 2000
+    lr: Learning Rate, by how many incremental steps will we back propagate. Default is 0.5
+    shuffle: should we shuffle the dataset, it helps the training behave like general learning rather than memorizing a fixed order. Default is True. 
+    """
+
+    # how many features (inputs in the dataset)
+    num_features = len(dataset[0][0])
+    # randomize the weights
+    w = [random.uniform(-1, 1) for _ in range(num_features)]
+    # set bias to 0.0
+    b = 0.0
+
+    for epoch in range(epochs):
+
+        # shuffle for better general learning
+        if shuffle:
+            random.shuffle(dataset)
+
+        total_loss = 0.0
+
+        # iterate over the dataset
+        for x, y in dataset:
+            _, a = forward(x, w, b)
+            total_loss += mse(y, a)
+
+            dL_dw, dL_db = backward(x, y, a)
+            w, b = update(w, b, dL_dw, dL_db, lr)
+
+        # for every 200 epochs print the avg_loss weights and bias
+        if (epoch + 1) % 200 == 0: 
+            avg_loss = total_loss / len(dataset)
+            print(f"epoch {epoch+1:4d} avg_loss={avg_loss:.6f} w={w} b={b:.4f}")
+
+    return w, b
+
+def predict(x, w, b, threshold=0.5):
+    """
+    Predict function outputs a hard 1.0 or 0.0 determined by value of the activation function
+    x: inputs
+    w: weights
+    b: bias
+    threshold: the determining factor for whether the prediction is 1.0 or 0.0
+    """
+    _, a = forward(x, w, b)
+
+    # return the activation function value and the prediction
+    return a, (1.0 if a >= threshold else 0.0)
 
 def mse(y, a):
     """
--- main.py
+++ main.py
@@ -1,41 +1,68 @@
 # from examples import first_pass
 import random
-from lib import forward, backward, update, mse
+from lib import forward, backward, update, mse, train, predict
 
-if __name__ == "__main__":
+# Forward and Backward Propagation
+# if __name__ == "__main__":
     
-    random.seed(0)
+#     random.seed(0)
 
-    # training set
-    x = [1.0, 0.0]
-    y = 1.0
+#     # training set
+#     x = [1.0, 0.0]
+#     y = 1.0
 
-    # random initialization
-    w = [random.uniform(-1, 1) for _ in range(len(x))]
-    b = 0.0
-    lr = 0.5
+#     # random initialization
+#     w = [random.uniform(-1, 1) for _ in range(len(x))]
+#     b = 0.0
+#     lr = 0.5
 
-    # before update
-    _, a1 = forward(x, w, b)
-    loss1 = mse(y, a1)
+#     # before update
+#     _, a1 = forward(x, w, b)
+#     loss1 = mse(y, a1)
 
-    # compute gradients and updates
-    dL_dw, dL_db = backward(x, y, a1)
-    w2, b2 = update(w, b, dL_dw, dL_db, lr)
+#     # compute gradients and updates
+#     dL_dw, dL_db = backward(x, y, a1)
+#     w2, b2 = update(w, b, dL_dw, dL_db, lr)
 
-    # after update
-    _, a2 = forward(x, w2, b2)
-    loss2 = mse(y, a2)
+#     # after update
+#     _, a2 = forward(x, w2, b2)
+#     loss2 = mse(y, a2)
 
-    print("\n\n\nBefore:")
-    print(" w = ", w, "b = ", b)
-    print(" pred = ", a1, "loss = ", loss1)
+#     print("\n\n\nBefore:")
+#     print(" w = ", w, "b = ", b)
+#     print(" pred = ", a1, "loss = ", loss1)
 
-    print("\nGradients:")
-    print(" dL_dw = ", dL_dw)
-    print(" dL_db = ", dL_db)
+#     print("\nGradients:")
+#     print(" dL_dw = ", dL_dw)
+#     print(" dL_db = ", dL_db)
 
-    print("\nAfter one update:")
-    print(" w = ", w2, " b = ", b2)
-    print(" pred = ", a2, " loss = ", loss2)
+#     print("\nAfter one update:")
+#     print(" w = ", w2, " b = ", b2)
+#     print(" pred = ", a2, " loss = ", loss2)
 
+# Training against the AND dataset and predicting the value
+if __name__ == "__main__":
+    random.seed(42)
+
+    # AND truth table - will succeed
+    dataset = [
+        ([0.0, 0.0], 0.0),
+        ([0.0, 1.0], 0.0),
+        ([1.0, 0.0], 0.0),
+        ([1.0, 1.0], 1.0),
+    ]
+
+    # XOR truth table will fail because XOR is not linearly seperable
+    # dataset = [
+    #     ([0.0, 0.0], 0.0),
+    #     ([0.0, 1.0], 1.0),
+    #     ([1.0, 0.0], 1.0),
+    #     ([1.0, 1.0], 0.0),
+    # ]
+
+    w, b = train(dataset, epochs=2000, lr=0.5)
+
+    print("\nFinal Test:")
+    for x, y in dataset:
+        a, yhat = predict(x, w, b)
+        print(f"x={x} y={y} pred_prob={a:.4f} pred_class={yhat}")
